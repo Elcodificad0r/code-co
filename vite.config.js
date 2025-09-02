9a25bfdca6eed6b3e9213968cdf2d68534d54c6f
Guido  Gueta <guidogueta@Guidos-MacBook-Pro.local>
2025-09-02 16:55:24 -0600

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/code-co/', // <--- Muy importante para GH Pages
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
